37909f55fb32a4a498ebfe25ad1dd192c8ff7b40
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-08-31 14:27:12 +0530

    Updates

---
+++ precache-manifest.03ca0ba97134caacf514502691a72af6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f569ee5d92df65bb5e0c4ca7c85147",
+    "url": "/pokeGallery/index.html"
+  },
+  {
+    "revision": "0323ccce45e90385fe71",
+    "url": "/pokeGallery/static/css/main.aadeb9ae.chunk.css"
+  },
+  {
+    "revision": "63b2059f7c2177e5f13b",
+    "url": "/pokeGallery/static/js/2.dc2253df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokeGallery/static/js/2.dc2253df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0323ccce45e90385fe71",
+    "url": "/pokeGallery/static/js/main.ae56f4c6.chunk.js"
+  },
+  {
+    "revision": "ca944b3357d050f89595",
+    "url": "/pokeGallery/static/js/runtime-main.4bb62273.js"
+  },
+  {
+    "revision": "ddd5084dff8d7606019645f62c1712c6",
+    "url": "/pokeGallery/static/media/pokelogo.ddd5084d.png"
+  }
+]);
